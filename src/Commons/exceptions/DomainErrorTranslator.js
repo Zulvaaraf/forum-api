655c83729470aeb666fbaff411e5bd178d6761a7
zulvaaraf <zulvasebelas@gmail.com>
2023-09-25 01:59:14 +0700

    thread(add): feature post thread implemented, which is before debug error in domain translator

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -17,6 +17,14 @@ DomainErrorTranslator._directories = {
   'REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
   'DELETE_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
   'DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
+
+  'ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat menambahkan thread baru karena property yang dibutuhkan tidak ada'),
+  'ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat menambahkan thread baru karena tipe data tidak sesuai'),
+  'ADD_THREAD.TITLE_LIMIT_CHAR': new InvariantError('tidak dapat menambahkan thread baru karena karakter title melebihi batas maksimal'),
+
+  // 'NEW_THREAD.NOT_CONTAIN_NEEDED.PROPERTY': new InvariantError('tidak dapat menambahkan thread baru karena properti yang dibutuhkan tidak ada'),
+  // 'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat menambahkan thread baru karena tipe data tidak sesuai'),
+  // 'NEW_THREAD.TITLE_LIMIT_CHAR': new InvariantError('tidak dapat menambahkan thread baru karena karakter title melebihi batas maksimal'),
 };
 
 module.exports = DomainErrorTranslator;
---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,86 @@
+const pool = require('../../database/postgres/pool');
+const container = require('../../container');
+const createServer = require('../createServer');
+const ServerTestHelper = require('../../../../tests/ServerTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+
+describe('/threads endpoint', () => {
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 401 when request did not have access token', async () => {
+      const requestPayload = {
+        title: 'judul',
+        body: 'isi body',
+      };
+
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+
+    it('should response 400 when post thread invalid body request', async () => {
+      const requestPayload = {
+        title: 'judul',
+      };
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat menambahkan thread baru karena property yang dibutuhkan tidak ada');
+    });
+
+    it('should response 201 and persisted thread', async () => {
+      const requestPayload = {
+        title: 'judul',
+        body: 'isi body',
+      };
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+      expect(responseJson.data.addedThread.title).toEqual(requestPayload.title);
+    });
+  });
+});
