7f49d82fa3e0b882dcd2ce3bcfc1c163c9446db0
zulvaaraf <zulvasebelas@gmail.com>
2023-09-27 20:46:58 +0700

    comments(add): add comment table test helper and modify threads table test

---
+++ tests/CommentTableTestHelper.js
@@ -0,0 +1,30 @@
+/* istanbul ignore file */
+
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentTableTestHelper = {
+  async addComment({ id = 'comment-123', threadId = 'thread-123', content = 'isi comment', owner = 'user-123', date = '2023-09-25T11:52:48.150Z', isDelete = false }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6) RETURNING id, thread_id, content, owner',
+      values: [id, threadId, content, owner, date, isDelete],
+    };
+
+    await pool.query(query);
+  },
+
+  async findCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1');
+  },
+};
+
+module.exports = CommentTableTestHelper;
