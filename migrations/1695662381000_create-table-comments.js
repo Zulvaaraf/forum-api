15b99b57880cfcc6140af3af817b99508418a2fb
zulvaaraf <zulvasebelas@gmail.com>
2023-09-27 20:52:12 +0700

    comments(migrations): fixing foreign key comments to threads

--- migrations/1695662381000_create-table-comments.js
+++ migrations/1695662381000_create-table-comments.js
@@ -26,8 +26,8 @@ exports.up = (pgm) => {
     },
   });
 
-  pgm.addConstraint('comments', 'fk_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
   pgm.addConstraint('comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments', 'fk_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
