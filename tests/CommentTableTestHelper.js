f4903facaa4c3ef8b0f76e63e5da22f33d216115
zulvaaraf <zulvasebelas@gmail.com>
2023-09-30 19:38:20 +0700

    tableHelper: change query

--- tests/CommentTableTestHelper.js
+++ tests/CommentTableTestHelper.js
@@ -5,7 +5,7 @@ const pool = require('../src/Infrastructures/database/postgres/pool');
 const CommentTableTestHelper = {
   async addComment({ id = 'comment-123', threadId = 'thread-123', content = 'isi comment', owner = 'user-123', date = '2023-09-25T11:52:48.150Z', isDelete = false }) {
     const query = {
-      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6) RETURNING id, thread_id, content, owner',
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6)',
       values: [id, threadId, content, owner, date, isDelete],
     };
 
