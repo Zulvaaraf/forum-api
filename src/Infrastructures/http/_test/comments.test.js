8dd541de807a1c8e990da91f9c6cecc3a6c96d1c
Zulva Baharrudin Araf <zulvasebelas@gmail.com>
2023-10-02 16:11:45 +0700

    comments(delete): test delete

--- src/Infrastructures/http/_test/comments.test.js
+++ src/Infrastructures/http/_test/comments.test.js
@@ -40,7 +40,7 @@ describe('/comments endpoint', () => {
       expect(responseJson.message).toEqual('Thread not found');
     });
 
-    it('should throw AuthorizationError when you are not autorized', async () => {
+    it('should response 401 when you are not autorized', async () => {
       const requestPayload = {
         content: 'isi comment',
       };
@@ -56,7 +56,68 @@ describe('/comments endpoint', () => {
       expect(responseJson.message).toEqual('Missing authentication');
     });
 
-    it('should add comment to thread with status code 201', async () => {
+    it('should response 400 when add comment not contain needed property', async () => {
+      const requestPayload = {};
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        title: 'isi title',
+        body: 'isi body',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat menambahkan comment baru karena property yang dibutuhkan tidak ada');
+    });
+
+    it('should response 400 when add comment not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        content: 123,
+      };
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        title: 'isi title',
+        body: 'isi body',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat menambahkan comment baru karena tipe data tidak sesuai');
+    });
+
+    it('should response 200 when add comment correctly', async () => {
       const requestPayload = {
         content: 'Need Help',
       };
@@ -88,6 +149,180 @@ describe('/comments endpoint', () => {
       expect(responseJson.data.addedComment.id).toBeDefined();
       expect(responseJson.data.addedComment.content).toBeDefined();
       expect(responseJson.data.addedComment.owner).toBeDefined();
+    });
+  });
+
+  describe('when DELETE /threads/{threadId}/comments/{commentId}', () => {
+    it('should response 404 when thread not found', async () => {
+      const requestPayload = {
+        content: 'isi comment',
+      };
+
+      const server = await createServer(container);
+      const accessToken = await ServerTestHelper.getAccessToken();
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/threads/thread-123/comments/comment-123',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('Thread not found!');
+    });
+
+    it('should response 404 when comment not found', async () => {
+      const requestPayload = {
+        content: 'isi comment',
+      };
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        title: 'judul',
+        body: 'isi body',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/threads/thread-123/comments/comment-123',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('Comments not found');
+    });
+
+    it('should response 403 when deleting non-belonging comments ', async () => {
+      const requestPayload = {
+        content: 'isi comment',
+      };
+
+      const server = await createServer(container);
+      const accessToken = await ServerTestHelper.getAccessToken();
+
+      await UsersTableTestHelper.addUser({
+        id: 'user-456',
+        username: 'johndoe',
+        password: 'inipasswordy',
+        fullname: 'John Doe',
+      });
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-456',
+        title: 'isi title',
+        body: 'isi body',
+        owner: 'user-456',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      await CommentTableTestHelper.addComment({
+        id: 'comment-456',
+        threadId: 'thread-456',
+        content: 'isi content',
+        owner: 'user-456',
+      });
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/threads/thread-456/comments/comment-456',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(403);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('You are not authorized to access');
+    });
+
+    it('should response 401 when request did not have accesToken', async () => {
+      const requestPayload = {
+        content: 'isi comment',
+      };
+
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      });
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        title: 'test title',
+        body: 'test body',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      await CommentTableTestHelper.addComment({
+        id: 'comment-123',
+        threadId: 'thread-123',
+        content: 'test content',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/threads/thread-123/comments/comment-123',
+        payload: requestPayload,
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+
+    it('should response 200 when delete comment correctly', async () => {
+      const requestPayload = {
+        content: 'test content',
+      };
+
+      const accessToken = await ServerTestHelper.getAccessToken();
+
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123',
+        title: 'test title',
+        body: 'test body',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+      await CommentTableTestHelper.addComment({
+        id: 'comment-123',
+        threadId: 'thread-123',
+        content: 'test content',
+        owner: 'user-123',
+        date: '2023-09-25T11:52:48.150Z',
+      });
+
+      const server = await createServer(container);
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/threads/thread-123/comments/comment-123',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual('success');
     });
   });
 });
